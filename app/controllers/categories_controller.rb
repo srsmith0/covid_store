4efd1abdf0af55b16787bd42ff8fbc78d76dabfd
Shawn Smith <srsmith11@live.com>
2020-06-06 12:31:26 -0600

    almost done with controller/lunchtime

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,37 @@
 class CategoriesController < ApplicationController
+  before_action :only [:new, :show, :destroy, :edit, :update]
+  
   def index
   end
 
   def new
+    @category.new = Category.create(category_params)
   end
 
   def show
-    @products = Product.find(product_params)
+    @products = Product.find(set_product)
   end
 
   def edit
+    
   end
+
+  def destroy
+    @category.delete
+  end
+
 
   private
 
-  def product_params
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
+
+  def set_category
+    Category.find(params[:id])
+  end
+
+  def set_product
     Product.find(params[:id])
   end
 
