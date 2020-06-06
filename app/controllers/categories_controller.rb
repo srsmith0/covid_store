9100190e5104d0f1b05b85ccfbaeeb10c3023861
Shawn Smith <srsmith11@live.com>
2020-06-06 14:05:57 -0600

    working on category index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,31 @@
 class CategoriesController < ApplicationController
-  before_action :only [:new, :show, :destroy, :edit, :update]
+  before_action only: [:new, :create, :show, :destroy, :edit, :update]
   
   def index
+    @categories = Category.all
   end
 
   def new
     @category.new = Category.create(category_params)
   end
 
+  def create
+    @category.new = Category.create(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new 
+  end
+end
+
+
   def show
+    @categories = Category.all
     @products = Product.find(set_product)
   end
 
   def edit
+    
     
   end
 
