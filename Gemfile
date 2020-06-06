9f16138b2a89468bad7a511a5009113a887d0be0
Shawn Smith <srsmith11@live.com>
2020-06-06 11:34:22 -0600

    added seeds and controllers

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'devise'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
@@ -36,6 +36,7 @@ end
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
+
   gem 'listen', '~> 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,38 +13,39 @@ Category.create(name:"Entertainment", description: "Don't get bored and support 
 Category.create(name:"Tolietries", description: "Stay fresh!")
 Category.create(name:"Food", description:"Don't go hungry during these difficult times.")
 
-Product.create(name: 'Face Mask', price: $ Faker::Number.decimal(l_digits: 2), category_id:1 )
-Product.create(name: 'Hand Sanitizer', price: $ Faker::Number.decimal(l_digits: 2), category_id:1 )
-Product.create(name: 'Cold Medicine', price: $ Faker::Number.decimal(l_digits: 2) , category_id:1 )
-Product.create(name: 'Netflix Subscription', price: $ Faker::Number.decimal(l_digits: 2) , category_id:2 )
-Product.create(name: 'Spotify Subscription', price: $ Faker::Number.decimal(l_digits: 2) , category_id:2 )
-Product.create(name: 'Audible Subscription', price: $ Faker::Number.decimal(l_digits: 2) , category_id:2 )
-Product.create(name: 'GrubHub Gift Card', price: $ Faker::Number.decimal(l_digits: 2) , category_id:2 )
-Product.create(name: 'Toothbrush', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Tooth Paste', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Shampoo', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Conditioner', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Extra Soft Toliet Paper', price: $ Faker::Number.decimal(l_digits: 2) , category_id: )
-Product.create(name: 'Hand Soap', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Face Soap', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Deordorant', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Shaving Cream', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Razor', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: "Lotion", price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
-Product.create(name: 'Case o\' Water', price: $ Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Face Mask', price:   Faker::Number.decimal(l_digits: 2), category_id:1 )
+Product.create(name: 'Hand Sanitizer', price:   Faker::Number.decimal(l_digits: 2), category_id:1 )
+Product.create(name: 'Cold Medicine', price:   Faker::Number.decimal(l_digits: 2) , category_id:1 )
+Product.create(name: 'Netflix Subscription', price:   Faker::Number.decimal(l_digits: 2) , category_id:2 )
+Product.create(name: 'Spotify Subscription', price:   Faker::Number.decimal(l_digits: 2) , category_id:2 )
+Product.create(name: 'Audible Subscription', price:  Faker::Number.decimal(l_digits: 2) , category_id:2 )
+Product.create(name: 'GrubHub Gift Card', price:   Faker::Number.decimal(l_digits: 2) , category_id:2 )
+Product.create(name: 'Toothbrush', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Tooth Paste', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Shampoo', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Conditioner', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Extra Soft Toliet Paper', price:   Faker::Number.decimal(l_digits: 2) , category_id: 3)
+Product.create(name: 'Hand Soap', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Face Soap', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Deordorant', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Shaving Cream', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Razor', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: "Lotion", price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
+Product.create(name: 'Case o\' Water', price:   Faker::Number.decimal(l_digits: 2) , category_id:3 )
 
 
 10.times do
   p_name = Faker::Food.fruits
   p_price = Faker::Number.decimal(l_digits: 2)
-  Product.create(name:p_name, p_price, category_id: 4)
+  Product.create(name:p_name, price: p_price, category_id: 4)
 end
 10.times do
   p_name = Faker::Food.dish
   p_price = Faker::Number.decimal(l_digits: 2)
-  Product.create(name:p_name, p_price, category_id: 4)
+  Product.create(name: p_name, price: p_price, category_id: 4)
 end
 10.times do
   p_name = Faker::Food.ingredient 
   p_price = Faker::Number.decimal(l_digits: 2)
   Product.create(name:p_name, price: p_price, category_id: 4)
+end
